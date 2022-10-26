f92611173065b795a9eeac443eba55cd47eefe30
Dursun <93613110+Dursunipek@users.noreply.github.com>
2022-10-26 20:58:40 +0300

    Added list of heroes with a detail view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,14 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name:  "Windstorm"
-  }
+  heroes = HEROES;
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
